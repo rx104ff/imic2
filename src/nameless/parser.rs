955a65e596d103ab39b7ddea93cfb9974d57effe
rx104ff <penelopoff@gmail.com>
2025-07-26 17:40:42 +0900

    Removed unused imports

--- src/nameless/parser.rs
+++ src/nameless/parser.rs
@@ -1,4 +1,4 @@
-use crate::{build_expression_parser, build_value_parser, common::{ast::{core::NamelessVar, judgement::Judgment, value::{NamedValue, NamelessValue}}, tokenizer::Token}, parser::{environment::traits::EnvironmentParser, BaseParser, ExpressionParser, ParserCore, ValueParser}};
+use crate::{build_expression_parser, build_value_parser, common::{ast::{core::NamelessVar, judgement::Judgment, value::{NamelessValue}}, tokenizer::Token}, parser::{environment::traits::EnvironmentParser, BaseParser, ExpressionParser, ParserCore, ValueParser}};
 
 pub struct Parser {
     core: ParserCore,
